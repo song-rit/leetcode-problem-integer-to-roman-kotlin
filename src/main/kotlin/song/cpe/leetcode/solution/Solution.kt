62d51a7baef784b1306e5540a2478551cb69d0e3
songrit maleerat <song.cpe@gmail.com>
2022-06-21 23:59:17 +0700

    Update logic

--- src/main/kotlin/song/cpe/leetcode/solution/Solution.kt
+++ src/main/kotlin/song/cpe/leetcode/solution/Solution.kt
@@ -14,7 +14,41 @@ class Solution {
 
     inline fun convertToInteger(romanNumber: String) = RomanNumber.valueOf(romanNumber).value
 
-    fun intToRoman(num: Int): String {
+    fun intToRoman(input: Int): String {
+
+        var num = input
+        while (num > 1) {
+            when {
+                num % convertToInteger(RomanNumber.M.name) == 0 -> {
+                    println(RomanNumber.M.name)
+                    num /= convertToInteger(RomanNumber.M.name)
+                }
+                num % convertToInteger(RomanNumber.D.name) == 0 -> {
+                    println(RomanNumber.D.name)
+                    num /= convertToInteger(RomanNumber.M.name)
+                }
+                num % convertToInteger(RomanNumber.C.name) == 0 -> {
+                    println(RomanNumber.C.name)
+                    num /= convertToInteger(RomanNumber.M.name)
+                }
+                num % convertToInteger(RomanNumber.L.name) == 0 -> {
+                    println(RomanNumber.L.name)
+                    num /= convertToInteger(RomanNumber.M.name)
+                }
+                num % convertToInteger(RomanNumber.X.name) == 0 -> {
+                    println(RomanNumber.X.name)
+                    num /= convertToInteger(RomanNumber.M.name)
+                }
+                num % convertToInteger(RomanNumber.V.name) == 0 -> {
+                    println(RomanNumber.V.name)
+                    num /= convertToInteger(RomanNumber.M.name)
+                }
+                num % convertToInteger(RomanNumber.I.name) == 0 -> {
+                    println(RomanNumber.I.name)
+                    num /= convertToInteger(RomanNumber.M.name)
+                }
+            }
+        }
         return ""
     }
 }
